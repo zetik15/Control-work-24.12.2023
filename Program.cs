166deb0bc10be014819409371a8c27bca48c8724
Makar <zetikspb@yandex.ru>
2023-12-24 11:38:10 +0300

    Через цикл for перебираем каждый элемент массива

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
 ﻿string[] Array = { ":(", "Saint-Petersburg", "GeekBrains", "GB", "78" };
 string str = string.Join(", ", Array);
 Console.Write($"[{str}] -> [");
+for (int i = 0; i < Array.Length; i++)
